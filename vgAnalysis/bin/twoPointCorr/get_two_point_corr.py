614eadf5797700596616b2776104d09659beeee9
sidShinde <siddhesh@umich.edu>
2017-08-10 11:46:31 -0400

    two point correlation utility added

---
+++ vgAnalysis/bin/twoPointCorr/get_two_point_corr.py
@@ -0,0 +1,111 @@
+import numpy as np
+import os
+from tqdm import tqdm
+from vgAnalysis.readers.reader_support_functions import *
+from vgAnalysis.readers.reader import *
+
+__all__ = ['get_two_point_corr']
+
+def get_two_point_corr(configFile):
+    configDict = config_to_dict(configFile)
+
+    # read data from configFile
+    filePath = os.getcwd()
+    filePath = filePath + '/postProcessing/cuttingPlane'
+
+    patchName = configDict['patchName']
+    nPlanes   = int( configDict['nPlanes'] )
+    delta     = float( configDict['delta'] )
+    yw        = float( configDict['yw'] )
+    nPts      = int( configDict['nPts'] )
+
+    # get list of time-dirs
+    nTimeDirs = int( configDict['nTimeDirs'] )
+    timeDirs  = os.listdir(filePath)
+    try:
+        timeDirs  = timeDirs[timeDirs.size - nTimeDirs:]
+    except:
+        print('\n insufficient time directories for averaging ...')
+
+    # dict to store two point correlation data
+    twoPointCorrX, twoPointCorrY, twoPointCorrZ = dict(), dict(), dict()
+    ycoord, zcoord = dict(), dict()
+
+    for i in range( nPlanes ):
+        print('     working on plane ' + str(i) + ' ...')
+
+        arrName = patchName + str(i)
+        twoPointCorrX[arrName], twoPointCorrY[arrName], twoPointCorrZ[arrName], \
+        ycoord[arrName], zcoord[arrName] = \
+        two_point_corr_matrix(filePath, arrName, timeDirs, delta, yw, nPts)
+
+    return twoPointCorrX, twoPointCorrY, twoPointCorrZ, ycoord, zcoord
+
+
+def two_point_corr_matrix(filePath, arrName, timeDirs, delta, yw, nPts):
+
+    for i in tqdm( range( len(timeDirs), ncols=100 ) ):
+        fpath  = filePath + '/' + timeDirs[i] + '/' + arrName
+        points = get_data( fpath + '/points', skiprows=3 )
+        U      = get_data( fpath + '/vectorField/U', skiprows=3)
+        UMean  = get_data( fpath + '/vectorField/UMean', skiprows=3)
+        UPrime = U - UMean
+
+        # interpolate data:
+        zcoord = np.unique( points[:, 2] ) / delta
+        ymin   = np.min( points[:, 1]/delta )
+        ycoord = np.linspace( ymin, yw, nPts )
+        zGrid, yGrid = np.meshgrid( zcoord, ycoord )
+
+        upx = griddata( points[:, 1:3], UPrime[:, 0], (zGrid, yGrid), method='linear')
+        upy = griddata( points[:, 1:3], UPrime[:, 1], (zGrid, yGrid), method='linear')
+        upz = griddata( points[:, 1:3], UPrime[:, 2], (zGrid, yGrid), method='linear')
+
+        # number of points in y and z:
+        [ny, nz] = upx.shape
+
+        if i == 0:
+            tpcX = np.zeros([ny, nz-1])
+            tpcY = np.zeros([ny, nz-1])
+            tpcZ = np.zeros([ny, nz-1])
+
+            tpcX = two_point_corr_eval(upx, ny, nz)
+            tpcY = two_point_corr_eval(upy, ny, nz)
+            tpcZ = two_point_corr_eval(upz, ny, nz)
+
+        else:
+            tpcX += two_point_corr_eval(upx, ny, nz)
+            tpcY += two_point_corr_eval(upy, ny, nz)
+            tpcZ += two_point_corr_eval(upz, ny, nz)
+
+    tpcX = tpcX/(i+1)
+    tpcY = tpcY/(i+1)
+    tpcZ = tpcZ/(i+1)
+
+    return tpcX, tpcY, tpcZ, ycoord, zcoord
+
+
+def two_point_corr_eval(up, ny, nz):
+    tpc     = np.zeros([ny, nz-1])
+    counter = np.zeros(nz-1)
+
+    for i in range(ny):
+        for j in range(nz):
+            # points on the left:
+            for lpts in range(0, j):
+                val = up[i, j] * up[i, lpts]
+                val /= np.square( up[i, j] )
+                tpc[i, abs(lpts-j)-1] += val
+                counter[ abs(lpts-j)-1 ] += 1
+
+            # points on the right:
+            for rpts in range(j+1, nz):
+                val = up[i, j] * up[i, rpts]
+                val /= np.square( up[i, j] )
+                tpc[i, abs(j-rpts)-1] += val
+                counter[ abs(j-rpts)-1 ] += 1
+
+            # normalization:
+            tpc[i, :] /= counter
+
+    return tpc
---
+++ vgAnalysis/bin/twoPointCorr/write_two_point_corr.py
@@ -0,0 +1,55 @@
+import numpy as np
+import os
+import argparse
+from vgAnalysis.readers.reader_support_functions import *
+from vgAnalysis.readers.reader import *
+from .get_two_point_corr import *
+
+def main():
+    parser = argparse.ArgumentParser(description='calculate spanwise two point \
+    correlation at specific streamwise locations')
+
+    parser.add_argument('-config',
+                        type=str,
+                        help='file with essential inputs',
+                        required=True)
+
+    args = parser.parse_args()
+
+    # parse the config:
+    configFile = open(args.config, mode='r')
+
+    twoPointCorrX, twoPointCorrY, twoPointCorrZ, ycoord, zcoord = \
+    get_two_point_corr(configFile)
+
+    caseDir = os.getcwd()
+    caseDir = caseDir + '/postProcessing/vgAnalysis/twoPointCoorData'
+    if not os.path.exists(caseDir):
+        os.mkdirs(caseDir)
+
+    planeNames = list( twoPointCorrX.keys() )
+
+    for name in planeNames:
+        fname = caseDir + '/two_point_coor_x_' + name + '.csv'
+        np.savetxt(fname, twoPointCorrX[name], fmt='%.3f', delimiter=', ',
+                   newline='\n')
+
+        fname = caseDir + '/two_point_coor_y_' + name + '.csv'
+        np.savetxt(fname, twoPointCorrY[name], fmt='%.3f', delimiter=', ',
+                   newline='\n')
+
+        fname = caseDir + '/two_point_coor_z_' + name + '.csv'
+        np.savetxt(fname, twoPointCorrZ[name], fmt='%.3f', delimiter=', ',
+                   newline='\n')
+
+        fname = caseDir + '/ycoord_' + name + '.csv'
+        np.savetxt(fname, ycoord[name], fmt='%.3f', delimiter=', ',
+                   newline='\n')
+
+        fname = caseDir + '/zcoord_' + name + '.csv'
+        np.savetxt(fname, zcoord[name], fmt='%.3f', delimiter=', ',
+                   newline='\n')
+
+
+if __name__ == '__main__':
+    main()
